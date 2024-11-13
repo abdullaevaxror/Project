086d1e57b34febafa7895370c78f77c1048e7359
abdullaevaxror <abdullayevaxror122@gmail.com>
2024-11-13 14:19:18 +0500

    Currency add

--- currency.php
+++ currency.php
@@ -3,7 +3,7 @@
 class Currency{
 
     const CURRENCY_API_URL = "https://cbu.uz/uz/arkhiv-kursov-valyut/json/";
-
+    public array $currencies = [];
     public function __construct(){
         
         $ch = curl_init();
@@ -12,7 +12,27 @@ class Currency{
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         $output = curl_exec($ch);
         curl_close($ch);
-        $decoded = json_decode(($output));
-        echo $decoded[0]->Ccy;
+        $this->currencies = json_decode(($output));
+    }
+
+    public function getCurrencies() : array {
+        $separeted_data = [];
+        $currencies_info = $this->currencies;
+        foreach($currencies_info as $currency) {
+            $separeted_data[$currency->Ccy] = $currency->Rate;
+
+        }
+        return $separeted_data;
+
+    }
+
+    public function exchange($value, $currency_name='USD') {
+
+        echo ceil($value / $this->getCurrencies()[$currency_name]) . ' ' . $currency_name;
+        
     }
 }
+
+$currency = new Currency();
+// 
+$currency->exchange(12800);
